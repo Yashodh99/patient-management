3fa92008034f47a5505e26eb9ec7a43c5e13fa04
yash.fern99 <yash.fern99@gmail.com>
2025-08-26 01:55:53 +0530

    fixed issues in pom.xml

--- billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
@@ -1,7 +1,19 @@
 package com.pm.billing_service.grpc;
 
+import billing.BillingResponse;
+import billing.BillingRequest;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
-}
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse>responseObserver) {
+
+    }
+
+
+
+    }
